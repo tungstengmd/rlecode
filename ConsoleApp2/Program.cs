4beef73959eadb6e50e095858944fb7efaf01065
harlow foxworthy <oooo58084@gmail.com>
2026-03-05 15:04:16 +0000

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿string[] rle = File.ReadAllLines("U:/logartRLE.txt");
+string body = "";
+for (int i = 0; i<rle.Length; i++) {
+    for (int j = 0; j<rle[i].Length; j+=3) for (int k = 0; k < Convert.ToInt32(rle[i][j..(j+2)]); k++) body+=rle[i][j+2];
+    body+="\n";
+}
+Console.WriteLine(body);
