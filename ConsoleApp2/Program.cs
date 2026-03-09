5086617da4f436932b89c48348bb83f02e3e299f
harlow foxworthy <oooo58084@gmail.com>
2026-03-09 11:09:29 +0000

    same thing but aqa would not understand

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,5 +1,5 @@
 ﻿string[] rle=File.ReadAllLines("U:/logartRLE.txt");
 for (int i=0;i<rle.Length;i++) {
-    for (int j=0;j<rle[i].Length;j+=3) for (int k=0;k<Convert.ToInt32(rle[i][j..(j+2)]);k++) Console.Write(rle[i][j+2]);
+    for (int j=0;j<rle[i].Length;j+=3) Console.Write(new string(rle[i][j+2],Convert.ToInt32(rle[i][j..(j+2)])));
     Console.WriteLine();
 }
